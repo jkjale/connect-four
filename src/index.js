a41a4550d3b9e10562d3a06c8d8f7d09d7349e1a
Jake Lee <leejakejh@gmail.com>
2022-11-08 20:13:24 -0800

    testing commits

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const Game = require('./game');
 function initApplication() {
     const game = new Game();
     const rootNode = game.getRootNodeForDimension();
-    
+    console.log('jaketest#@#@#------33333');
 }
 
 if (document.readyState === 'loading') {
